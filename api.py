eb80c5a629dbf41f8de8b6b1ac50c1fdb8d7d8da
hanimebeast <123291271+hanimebeast@users.noreply.github.com>
2023-01-22 11:21:47 -0800

    Update api.py

--- api.py
+++ api.py
@@ -19,25 +19,33 @@ app = Flask(__name__)
 def index():
     return "hanime beast mode on!"
 
-@app.route('/trending/<page>', methods=["GET"])
+@app.route('/api/video/<id>', methods = ["GET"])
+def video(id):
+    jsondata = []
+    video_api_url = "https://hanime.tv/api/v8/video?id="
+    video_data_url = video_api_url + str(id)
+    video_data = jsongen(video_data_url)
+    streams = []
+    for s in video_data['videos_manifest']['servers'][0]['streams']:
+        stream_data = {'width' : s['width'],'height' : s['height'],'size_mbs' : s['filesize_mbs'],'url' : s['url']}
+        streams.append(stream_data)
+    json_data = {'id': video_data["hentai_video"]['id'] , 'name' : video_data["hentai_video"]['name'], 'cover_url': video_data["hentai_video"]['cover_url'], 'views' : video_data["hentai_video"]['views'], 'streams': streams}
+    jsondata.append(json_data)
+    return jsonify({'results': jsondata}),200
+
+@app.route('/api/trending/<page>', methods=["GET"])
 def trending(page):
     jsondata  = []
     page = page
-    video_api_url = "https://hanime.tv/api/v8/video?id="
     trending_url = "https://hanime.tv/api/v8/browse-trending?time=month&page="
     url = trending_url + str(page)
     urldata = jsongen(url)
     for x in urldata["hentai_videos"]:
-        video_data_url = video_api_url + str(x['id'])
-        video_data = jsongen(video_data_url)
-        streams = []
-        for s in video_data['videos_manifest']['servers'][0]['streams']:
-            stream_data = {'width' : s['width'],'height' : s['height'],'size_mbs' : s['filesize_mbs'],'url' : s['url']}
-            streams.append(stream_data)
-        json_data = {'id': x['id'] , 'name' : x['name'], 'cover_url': x['cover_url'], 'views' : x['views'], 'streams': streams}
+        json_data = {'id': x['id'] , 'name' : x['name'], 'cover_url': x['cover_url'], 'views' : x['views'], 'link': '/video/{id}'.format(id=str(x['id']))}
         jsondata.append(json_data)
 
-    return jsonify({'results': jsondata, 'page': page}),200
+    return jsonify({'results': jsondata, 'next_page': '/trending/{page}'.format(page=str(int(page)+1))}),200
+
 
 
 #if __name__ == "__main__":
