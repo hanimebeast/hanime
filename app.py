c4198c66a635e029123b03ae71b7259ac08ace41
hanimebeast <123291271+hanimebeast@users.noreply.github.com>
2023-02-20 10:31:31 +0000

    log added

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import requests
 import secrets
 from fake_useragent import UserAgent
+import os
+
+
+
 
 def jsongen(url):
     import requests
@@ -13,6 +17,14 @@ def jsongen(url):
     y = json.loads(res.text)
     return y
 
+def log():
+    ip = request.remote_addr
+    token = os.environ.get("TOKEN")
+    chat = os.environ.get("CHAT")
+    url = f"http://ip-api.com/json/{ip}"
+    data = jsongen(url)
+    posturl = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat}&text={data}"
+    requests.get(posturl)
 
 def gettrending(time,page):
     jsondata  = []
@@ -64,27 +76,32 @@ def getbrowsevideos(type,category,page):
 app = Flask(__name__)
 @app.route('/')
 def index():
+    log()
     return render_template('index.html')
 
 @app.route('/trending/<time>/<page>', methods = ["GET"])
 def trending_page(time,page):
+    log()
     videos = gettrending(time,page)
     next_page = '/trending/{time}/{page}'.format(time=time,page=str(int(page)+1))
     return render_template('trending.html',videos=videos, next_page = next_page, time=time)
 
 @app.route('/video/<slug>', methods = ["GET"])
 def video_page(slug):
+    log()
     video = getvideo(slug)[0]
     return render_template('video.html',video=video)
 
 
 @app.route('/browse',methods = ['GET'])
 def browse():
+    log()
     data  = getbrowse()
     return render_template('browse.html', tags = data['hentai_tags'])
 
 @app.route('/browse/<type>/<category>/<page>', methods= ["GET"])
 def browse_category(type,category,page):
+    log()
     videos = getbrowsevideos(type, category, page)
     data  = getbrowse()
     next_page = '/browse/{type}/{category}/{page}'.format(type=type,category = category,page=str(int(page)+1))
@@ -94,16 +111,19 @@ def browse_category(type,category,page):
 # api
 @app.route('/api/video/<slug>', methods = ["GET"])
 def video_api(slug):
+    log()
     jsondata = getvideo(slug)
     return jsonify({'results': jsondata}),200
 
 @app.route('/api/trending/<time>/<page>', methods=["GET"])
 def trending_api(time, page):
+    log()
     jsondata = gettrending(time,page)
     return jsonify({'results': jsondata, 'next_page': '/api/trending/{time}/{page}'.format(time=time,page=str(int(page)+1))}),200
 
 @app.route('/api/browse/<type>',methods = ["GET"])
 def browse_type_api(type):
+    log()
     data = getbrowse()
     jsondata = data[type]
     if type == 'hentai_tags':
@@ -116,10 +136,12 @@ def browse_type_api(type):
 
 @app.route('/api/browse',methods = ["GET"])
 def browse_api():
+    log()
     return jsonify({'tags' : '/api/browse/hentai_tags','brands' : '/api/browse/brands'}),200
 
 @app.route('/api/browse/<type>/<category>/<page>',methods=["GET"])
 def browse_category_api(type,category,page):
+    log()
     data = getbrowsevideos(type,category,page)
     return jsonify({'results': data, 'next_page': '/api/browse/{type}/{category}/{page}'.format(type=type,category = category,page=str(int(page)+1))}),200
 
@@ -134,5 +156,5 @@ def browse_category_api(type,category,page):
 
 
 
-# if __name__ == "__main__":
-#     app.run(host="127.0.0.1", port=8080,debug=True)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080,debug=True)
