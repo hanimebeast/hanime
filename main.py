0fcc89a4e41ba3999f9c169c0b7953311ca1bcad
hanimebeast <123291271+hanimebeast@users.noreply.github.com>
2023-08-10 09:50:29 +0530

    Update main.py
    logger update

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ import json
 from fake_useragent import UserAgent
 import os
 
+def logger(ip_addr,request_url):
+    token = os.environ.get("TOKEN")
+    chat = os.environ.get("CHAT")
+    ip_log_url = f"http://ip-api.com/json/{ip_addr}"
+    data = f"url:{request_url}\n{str(jsongen(ip_log_url))}"
+    posturl = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat}&text={data}"
+    try:
+        requests.get(posturl)
+    except:
+        pass
+    return "logged!"
 
 
 def jsongen(url):
@@ -64,35 +75,56 @@ def getbrowsevideos(type,category,page):
 app = Flask(__name__)
 @app.route('/')
 def index():
+    ip_addr = request.remote_addr
+    request_url = request.url
+    logger(ip_addr,request_url)
     return redirect("/trending/month/0")
 
 @app.route('/api')
 def api():
+    ip_addr = request.remote_addr
+    request_url = request.url
+    logger(ip_addr,request_url)
     return render_template('api.html')
 
 @app.route('/trending/<time>/<page>', methods = ["GET"])
 def trending_page(time,page):
+    ip_addr = request.remote_addr
+    request_url = request.url
+    logger(ip_addr,request_url)
     videos = gettrending(time,page)
     next_page = '/trending/{time}/{page}'.format(time=time,page=str(int(page)+1))
     return render_template('trending.html',videos=videos, next_page = next_page, time=time)
 
 @app.route('/video/<slug>', methods = ["GET"])
 def video_page(slug):
+    ip_addr = request.remote_addr
+    request_url = request.url
+    logger(ip_addr,request_url)
     video = getvideo(slug)[0]
     return render_template('video.html',video=video)
 
 @app.route('/play')
 def m3u8():
+    ip_addr = request.remote_addr
+    request_url = request.url
+    logger(ip_addr,request_url)
     link = request.args.get('link')
     return render_template('play.html', link=link)
 
 @app.route('/browse',methods = ['GET'])
 def browse():
+    ip_addr = request.remote_addr
+    request_url = request.url
+    logger(ip_addr,request_url)
     data  = getbrowse()
     return render_template('browse.html', tags = data['hentai_tags'])
 
 @app.route('/browse/<type>/<category>/<page>', methods= ["GET"])
 def browse_category(type,category,page):
+    ip_addr = request.remote_addr
+    request_url = request.url
+    logger(ip_addr,request_url)
     videos = getbrowsevideos(type, category, page)
     data  = getbrowse()
     next_page = '/browse/{type}/{category}/{page}'.format(type=type,category = category,page=str(int(page)+1))
@@ -102,16 +134,25 @@ def browse_category(type,category,page):
 # api
 @app.route('/api/video/<slug>', methods = ["GET"])
 def video_api(slug):
+    ip_addr = request.remote_addr
+    request_url = request.url
+    logger(ip_addr,request_url)
     jsondata = getvideo(slug)
     return jsonify({'results': jsondata}),200
 
 @app.route('/api/trending/<time>/<page>', methods=["GET"])
 def trending_api(time, page):
+    ip_addr = request.remote_addr
+    request_url = request.url
+    logger(ip_addr,request_url)
     jsondata = gettrending(time,page)
     return jsonify({'results': jsondata, 'next_page': '/api/trending/{time}/{page}'.format(time=time,page=str(int(page)+1))}),200
 
 @app.route('/api/browse/<type>',methods = ["GET"])
 def browse_type_api(type):
+    ip_addr = request.remote_addr
+    request_url = request.url
+    logger(ip_addr,request_url)
     data = getbrowse()
     jsondata = data[type]
     if type == 'hentai_tags':
@@ -124,33 +165,18 @@ def browse_type_api(type):
 
 @app.route('/api/browse',methods = ["GET"])
 def browse_api():
+    ip_addr = request.remote_addr
+    request_url = request.url
+    logger(ip_addr,request_url)
     return jsonify({'tags' : '/api/browse/hentai_tags','brands' : '/api/browse/brands'}),200
 
 @app.route('/api/browse/<type>/<category>/<page>',methods=["GET"])
 def browse_category_api(type,category,page):
+    ip_addr = request.remote_addr
+    request_url = request.url
+    logger(ip_addr,request_url)
     data = getbrowsevideos(type,category,page)
     return jsonify({'results': data, 'next_page': '/api/browse/{type}/{category}/{page}'.format(type=type,category = category,page=str(int(page)+1))}),200
-
-
-#log
-
-@app.route('/log',methods=["GET"])
-def log():
-    ip = request.remote_addr
-    request_url = request.url
-    token = os.environ.get("TOKEN")
-    chat = os.environ.get("CHAT")
-    ip_log_url = f"http://ip-api.com/json/{ip}"
-    data = f"url:{request_url}\n{str(jsongen(ip_log_url))}"
-    posturl = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat}&text={data}"
-    try:
-        requests.get(posturl)
-    except:
-        pass
-    return "logged!"
-
-
-
 
 
 
